bd40a96fd45eeea9c6e1e60dff1a7c57315d5549
이건희 <79892675+LOOPARAM@users.noreply.github.com>
2025-04-25 19:51:53 +0900

    fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,14 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "내신 등급컷 예측기",
   description: "우리학교 등급컷은?",
+  icons: {
+    // 기본 브라우저 탭 아이콘
+    icon: "/icon2.png",
+    // 크롬 등에서 사용하는 shortcut icon
+    shortcut: "/icon2.png",
+    // iOS 홈 화면 아이콘
+    apple: "/icon2.png",
+  },
 };
 
 export default function RootLayout({
